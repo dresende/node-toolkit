097ea3b5225a1e998b91c37d28e8cb2ff210c509
Diogo Resende <dresende@thinkdigital.pt>
2011-11-01 21:45:10 +0000

    fixed array and number extensions; added some tests for these 2

---
+++ tests/array.js
@@ -0,0 +1,87 @@
+require("./../lib/index").extendNative();
+
+var vows = require("vows"),
+    assert = require("assert");
+
+vows.describe("array").addBatch({
+	"The array [1,2,3,4,5]": {
+		topic: [1,2,3,4,5],
+		"first()": function (topic) {
+			assert.equal(topic.first(), 1);
+		},
+		"last()": function (topic) {
+			assert.equal(topic.last(), 5);
+		},
+		"without()": function (topic) {
+			var a = topic.without();
+			assert.equal(a.length, 5);
+			assert.equal(a[0], 1);
+			assert.equal(a[1], 2);
+			assert.equal(a[2], 3);
+			assert.equal(a[3], 4);
+			assert.equal(a[4], 5);
+		},
+		"without(3)": function (topic) {
+			var a = topic.without(3);
+			assert.equal(a.length, 4);
+			assert.equal(a[0], 1);
+			assert.equal(a[1], 2);
+			assert.equal(a[2], 4);
+			assert.equal(a[3], 5);
+		},
+		"without(3, 2)": function (topic) {
+			var a = topic.without(3, 2);
+			assert.equal(a.length, 3);
+			assert.equal(a[0], 1);
+			assert.equal(a[1], 4);
+			assert.equal(a[2], 5);
+		},
+		"sum()": function (topic) {
+			assert.equal(topic.sum(), 15);
+		},
+		"product()": function (topic) {
+			assert.equal(topic.product(), 120);
+		},
+		"min()": function (topic) {
+			assert.equal(topic.min(), 1);
+		},
+		"max()": function (topic) {
+			assert.equal(topic.max(), 5);
+		},
+		"grep(/[23]/)": function (topic) {
+			var a = topic.grep(/[23]/);
+			assert.equal(a.length, 2);
+			assert.equal(a[0], 2);
+			assert.equal(a[1], 3);
+		}
+	}
+}).addBatch({
+	"The array [1,2,null,3,,2,1,undefined]": {
+		topic: [1,2,null,3,,2,1,undefined],
+		"compact()": function (topic) {
+			var a = topic.compact();
+			assert.equal(a.length, 5);
+			assert.equal(a[0], 1);
+			assert.equal(a[1], 2);
+			assert.equal(a[2], 3);
+			assert.equal(a[3], 2);
+			assert.equal(a[4], 1);
+		},
+		"unique()": function (topic) {
+			var a = topic.unique();
+			assert.equal(a.length, 5);
+			assert.equal(a[0], 1);
+			assert.equal(a[1], 2);
+			assert.equal(a[2], null);
+			assert.equal(a[3], 3);
+			assert.equal(a[4], undefined);
+		},
+		"compact().unique()": function (topic) {
+			var a = topic.compact().unique();
+			assert.equal(a.length, 3);
+			assert.equal(a[0], 1);
+			assert.equal(a[1], 2);
+			assert.equal(a[2], 3);
+		}
+	}
+}).export(module);
---
+++ tests/number.js
@@ -0,0 +1,83 @@
+require("./../lib/index").extendNative();
+
+var vows = require("vows"),
+    assert = require("assert");
+
+vows.describe("number").addBatch({
+	"The number 1234.567": {
+		topic: 1234.567,
+		"format(2, '\"', ' ')": function (topic) {
+			assert.equal(topic.format(2, ",", " "), "1 234,57");
+		},
+		"round(2)": function (topic) {
+			assert.equal(topic.round(2), 1234.57);
+		},
+		"ceil()": function (topic) {
+			assert.equal(topic.ceil(), 1235);
+		},
+		"floor()": function (topic) {
+			assert.equal(topic.floor(), 1234);
+		},
+		"abs()": function (topic) {
+			assert.equal((-topic).abs(), topic);
+		},
+		"odd()": function (topic) {
+			assert.ok(!topic.odd());
+		},
+		"even()": function (topic) {
+			assert.ok(topic.even());
+		},
+		"pow(2)": function (topic) {
+			assert.equal(topic.pow(2), 1524155.677489);
+		},
+		"duration": function (topic) {
+			assert.equal((topic * 1e3).duration(), "20h 34s");
+		}
+	}
+}).addBatch({
+	"A range(10)": {
+		topic: Number.range(10),
+		"has 10 elements": function (topic) {
+			assert.equal(topic.length, 10);
+		},
+		"it's 0..9": function (topic) {
+			for (var i = 0; i < topic.length; i++)
+				assert.equal(topic[i], i);
+		}
+	},
+	"A range(5, 10)": {
+		topic: Number.range(5, 10),
+		"has 5 elements": function (topic) {
+			assert.equal(topic.length, 5);
+		},
+		"it's 5..9": function (topic) {
+			assert.equal(topic[0], 5);
+			assert.equal(topic[1], 6);
+			assert.equal(topic[2], 7);
+			assert.equal(topic[3], 8);
+			assert.equal(topic[4], 9);
+		}
+	},
+	"A range(5, 10, 2)": {
+		topic: Number.range(5, 10, 2),
+		"has 3 elements": function (topic) {
+			assert.equal(topic.length, 3);
+		},
+		"it's 5,7,9": function (topic) {
+			assert.equal(topic[0], 5);
+			assert.equal(topic[1], 7);
+			assert.equal(topic[2], 9);
+		}
+	},
+	"A range(-5, -10, -2)": {
+		topic: Number.range(-5, -10, -2),
+		"has 3 elements": function (topic) {
+			assert.equal(topic.length, 3);
+		},
+		"it's -5,-7,-9": function (topic) {
+			assert.equal(topic[0], -5);
+			assert.equal(topic[1], -7);
+			assert.equal(topic[2], -9);
+		}
+	}
+}).export(module);
